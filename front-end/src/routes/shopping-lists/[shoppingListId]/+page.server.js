667f5c73722f94a1fdec7a8fb04abcabe0be9655
Nazar Bachynskyy <nazarbach@gmail.com>
2023-01-14 00:25:34 +0100

    .env change

--- front-end/src/routes/shopping-lists/[shoppingListId]/+page.server.js
+++ front-end/src/routes/shopping-lists/[shoppingListId]/+page.server.js
@@ -2,7 +2,7 @@ import { SECRET_CATALOGUE_SERVICE_URL } from '$env/static/private'
 
 
 export async function load({params}){
-    const res = await fetch(`${SECRET_CATALOGUE_SERVICE_URL}/api/catalogues/${params.catalogueId}`)
+    const res = await fetch(`${SECRET_SHOPPING_LIST_SERVICE_URL}/api/shopping-lists/${params.catalogueId}`)
     const data = await res.json()
     return data
 }
--- front-end/src/routes/shopping-lists/[shoppingListId]/+page.svelte
+++ front-end/src/routes/shopping-lists/[shoppingListId]/+page.svelte
@@ -5,13 +5,13 @@
 </script>
 
 <a href="/">Back</a>
-<h1>Shopping ist</h1>
-<a href="/catalogues/{$page.params.catalogueId}/create">Create</a>
+<h1>Shopping list</h1>
 <div>
-  {#each data.catalogue.items as item}
+  {#each data.shoppingList.active_items as item}
     <div>
       <h2><a href="/catalogues/{$page.params.catalogueId}/items/{item.id}">{item.name}</a></h2>
-      <p>{item.quantity}{item.unit}</p>
+      <p>{item.amount}</p>
+      <p>{item.added_at}</p>
     </div>
   {/each}
 </div>
