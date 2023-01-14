624dadd80bfb804e59fae277c9c11d1366609007
Nazar Bachynskyy <nazarbach@gmail.com>
2023-01-14 23:38:53 +0100

    nice 2

--- front-end/src/routes/shopping-lists/[shoppingListId]/+page.server.js
+++ front-end/src/routes/shopping-lists/[shoppingListId]/+page.server.js
@@ -5,6 +5,7 @@ export async function load({params}){
     console.log(`${SECRET_SHOPPING_LIST_SERVICE_URL}/api/shopping-lists/${params.shoppingListId}`)
     const res = await fetch(`${SECRET_SHOPPING_LIST_SERVICE_URL}/api/shopping-lists/${params.shoppingListId}`)
     const data = await res.json()
+    console.log(data.shoppingList.items)
     return data
 }
 
@@ -25,5 +26,32 @@ export const actions = {
         fetch(`${SECRET_SHOPPING_LIST_SERVICE_URL}/api/shopping-lists/${formData.get('shoppingListId')}/active-items/${formData.get('activeItemId')}/decrease`, {
             method: 'POST',
         })
+    },
+
+    addActiveItem: async ({request}) => {
+        const formData = await request.formData()
+        console.log(formData)
+        console.log(`${SECRET_SHOPPING_LIST_SERVICE_URL}/api/shopping-lists/${formData.get('shoppingListId')}/active-items`)
+        const response = await fetch(`${SECRET_SHOPPING_LIST_SERVICE_URL}/api/shopping-lists/${formData.get('shoppingListId')}/active-items`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            body: JSON.stringify({
+                item_id: formData.get('items-input'),
+                shopping_list_id: formData.get('shoppingListId'),
+                amount: 1
+            })
+        })
+
+        console.log( await response)
+        const data =  await response.json()
+        console.log(data)
+        return {
+            success: true,
+            data
+        }
     }
 }
--- front-end/src/routes/shopping-lists/[shoppingListId]/+page.svelte
+++ front-end/src/routes/shopping-lists/[shoppingListId]/+page.svelte
@@ -1,11 +1,21 @@
 <script>
   import { page } from '$app/stores';
-  export let addServer;
   export let data;
 </script>
 
 <a href="/">Back</a>
 <h1>Shopping list</h1>
+<form method="POST" formaction="?/addActiveItem" >
+  <label for="items">Search</label>
+  <input list="items" id="items-input" name="items-input">
+  <input name="shoppingListId" type="hidden" value="{$page.params.shoppingListId}">
+  <datalist id="items">
+    {#each data.shoppingList.items as item}
+      <option value="{item.id}" label="{item.name}" >{item.name}</option>
+    {/each}
+  </datalist>
+  <button formaction="?/addActiveItem">Add</button>
+</form>
 
 <div>
   {#each data.shoppingList.active_items as active_item}
